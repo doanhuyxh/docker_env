3edfdab20036021c7b253cabd15c60cc251a9dba
doanhuyxh <doanhuyxh@gmail.com>
2025-03-01 11:55:23 +0700

    update

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -2,4 +2,8 @@
 docker pull mongo
 
 # Run a new container with the pulled image
-docker run --name mongo -p 27017:27017 -d mongo
+docker run -d --name mongo_database \
+  -e MONGO_INITDB_ROOT_USERNAME=admin \
+  -e MONGO_INITDB_ROOT_PASSWORD=01882904300Huy@ \
+  -p 27017:27017 \
+  mongo
